f1a61f87fbbd26f54cccf62e1529de22629ba47e
annaVih <anna-kobzii@mail.ru>
2018-12-28 00:14:59 +0000

    Updates

---
+++ precache-manifest.a1b28ae620349d5e050925dfa91c9be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03c54236dbf157ac1b9",
+    "url": "/simpleSeasonsApp/static/js/runtime~main.b03c5423.js"
+  },
+  {
+    "revision": "53103ca9708f1f5d4101",
+    "url": "/simpleSeasonsApp/static/js/main.53103ca9.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/simpleSeasonsApp/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "53103ca9708f1f5d4101",
+    "url": "/simpleSeasonsApp/static/css/main.a97e7986.chunk.css"
+  },
+  {
+    "revision": "25a3eb03478a22a5e60d880bef82a389",
+    "url": "/simpleSeasonsApp/index.html"
+  }
+];
